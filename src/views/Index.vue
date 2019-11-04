9ef7433e4d339ca11452141b9b5ccebe77970ae7
wangweiyu <woshiwwy16@126.com>
2019-11-04 17:13:46 +0800

    initial commit

---
+++ src/views/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <Button type="primary" loading>默认按钮</Button>
+  </div>
+</template>
+
+<script>
+import { Button } from 'vant'
+export default {
+  name: 'Index',
+  components: {
+    Button
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
